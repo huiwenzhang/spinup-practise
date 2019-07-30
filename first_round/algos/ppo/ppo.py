13e98926b56dd33f0cd56eddb15c224e7d0b201f
Alvin <huiwen3304@gmail.com>
2019-07-30 20:50:53 +0800

    PPO support

---
+++ first_round/algos/ppo/ppo.py
@@ -0,0 +1,237 @@
+import numpy as np
+import tensorflow as tf
+import gym
+import time
+import first_round.algos.ppo.core as core
+from first_round.utils.logx import EpochLogger
+from first_round.utils.mpi_tf import MpiAdamOptimizer, sync_all_params
+from first_round.utils.mpi_tools import mpi_fork, mpi_avg, proc_id, mpi_statistics_scalar, num_procs
+
+
+class PPOBuffer:
+    def __init__(self, obs_dim, act_dim, size, gamma=0.99, lam=0.95):
+        self.obs_buf = np.zeros(core.combined_shape(size, obs_dim), dtype=np.float32)
+        self.act_buf = np.zeros(core.combined_shape(size, act_dim), dtype=np.float32)
+        self.adv_buf = np.zeros(size, dtype=np.float32)
+        self.rew_buf = np.zeros(size, dtype=np.float32)
+        self.ret_buf = np.zeros(size, dtype=np.float32)
+        self.val_buf = np.zeros(size, dtype=np.float32)
+        self.logp_buf = np.zeros(size, dtype=np.float32)
+        self.gamma, self.lam = gamma, lam
+        self.ptr, self.path_start_idx, self.max_size = 0, 0, size
+
+    def store(self, obs, act, rew, val, logp):
+        assert self.ptr < self.max_size
+        self.obs_buf[self.ptr] = obs
+        self.act_buf[self.ptr] = act
+        self.rew_buf[self.ptr] = rew
+        self.val_buf[self.ptr] = val
+        self.logp_buf[self.ptr] = logp
+        self.ptr += 1
+
+    def finish_path(self, last_val=0):
+        path_slice = slice(self.path_start_idx, self.ptr)
+        rews = np.append(self.rew_buf[slice], last_val)
+        vals = np.append(self.val_buf[slice], last_val)
+
+        deltas = rews[:-1] + self.gamma * vals[1:] - vals[:-1]
+        self.adv_buf[path_slice] = core.discount_cumsum(deltas, self.gamma * self.lam)
+
+        self.ret_buf[slice] = core.discount_cumsum(rews, self.gamma)[:-1]
+
+        self.path_start_idx = self.ptr
+
+    def get(self):
+        assert self.ptr == self.max_size
+        self.ptr, self.path_start_idx = 0, 0
+        # Normalization
+        adv_mean, adv_std = mpi_statistics_scalar(self.adv_buf)
+        self.adv_buf = (self.adv_buf - adv_mean) / adv_std
+        return [self.obs_buf, self.act_buf, self.adv_buf, self.ret_buf, self.logp_buf]
+
+
+"""
+PPO, clipping
+"""
+
+
+def ppo(env_fn, actor_critic=core.mlp_actor_critic, ac_kwargs=dict(), seed=0,
+        steps_per_epoch=4000, epochs=50, gamma=0.99, clip_ratio=0.2, pi_lr=3e-4,
+        vf_lr=1e-3, trian_pi_iters=80, train_v_iters=80, lam=0.97, max_ep_len=1000,
+        target_kl=0.01, logger_kwargs=dict(), save_freq=10):
+    """
+
+    :param env_fn:
+    :param actor_critic:
+    :param ac_kwargs:
+    :param seed:
+    :param steps_per_epoch:
+    :param epochs:
+    :param gamma:
+    :param clip_ratio:
+    :param pi_lr:
+    :param vf_lr:
+    :param trian_pi_iters:
+    :param train_v_iters:
+    :param lam:
+    :param max_ep_len:
+    :param target_kl:
+    :param logger_kwargs:
+    :param save_freq:
+    :return:
+    """
+    logger = EpochLogger(**logger_kwargs)
+    logger.save_config(locals())
+
+    seed += 10000 * proc_id()
+    tf.set_random_seed(seed)
+    np.random.seed(seed)
+
+    env = env_fn()
+    obs_dim = env.observation_space.shape
+    act_dim = env.action_space.shape
+
+    # Shared information about action space
+    ac_kwargs['action_space'] = env.action_space
+
+    # Placeholders
+    x_ph, a_ph = core.placeholder_from_spaces(env.observation_space, env.action_space)
+    adv_ph, ret_ph, logp_old_ph = core.placeholders(None, None, None)
+
+    # Network
+    pi, logp, logp_pi, v = actor_critic(x_ph, a_ph, **ac_kwargs)
+
+    all_phs = [x_ph, a_ph, adv_ph, ret_ph, logp_old_ph]
+
+    get_action_ops = [pi, v, logp_pi]
+
+    # Experience buffer
+    local_steps_per_epoch = tuple(steps_per_epoch / num_procs())
+    buf = PPOBuffer(obs_dim, act_dim, local_steps_per_epoch, gamma, lam)
+
+    # Count Variables
+    var_counts = tuple(core.count_vars(scope) for scope in ['pi', 'v'])
+    logger.log('\nNumber of parameters: \t pi: %d, \t v: %d\n' % var_counts)
+
+    # PPO objectives
+    ratio = tf.exp(logp - logp_old_ph)
+    min_adv = tf.where(adv_ph > 0, (1 + clip_ratio) * adv_ph, (1 - clip_ratio) * adv_ph)
+    pi_loss = -tf.reduce_mean(tf.minimum(ratio * adv_ph, min_adv))
+    v_loss = tf.reduce_mean((ret_ph - v) ** 2)
+
+    # Auxliary variable
+    approx_kl = tf.reduce_mean(logp_old_ph - logp)
+    approx_ent = tf.reduce_mean(-logp)
+    clipped = tf.logical_or(ratio > (1 + clip_ratio), ratio < (1 - clip_ratio))
+    clipfrac = tf.reduce_mean(tf.cast(clipped, tf.float32))
+
+    # Optimizers
+    train_pi = MpiAdamOptimizer(learning_rate=pi_lr).minimize(pi_loss)
+    train_v = MpiAdamOptimizer(learning_rate=vf_lr).minimize(v_loss)
+
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    sess = tf.Session(config=config)
+    sess.run(tf.global_variables_initializer())
+
+    # Sync
+    sess.run(sync_all_params())
+
+    # Saver setup
+    logger.setup_tf_saver(sess, inputs={'x': x_ph}, outputs={'pi': pi, 'v': v})
+
+    def update():
+        # inputs dicts
+        inputs = {k: v for k, v in zip(all_phs, buf.get())}
+        pi_l_old, v_l_old, ent = sess.run([pi_loss, v_loss, approx_ent], feed_dict=inputs)
+
+        for i in range(trian_pi_iters):
+            _, kl = sess.run([train_pi, approx_kl], feed_dict=inputs)
+            kl = mpi_avg(kl)
+            if kl > 1.5 * target_kl:
+                logger.log('Early stopping at step {} due to reaching max kl.'.format(i))
+                break
+        logger.store(StopIter=i)
+        for _ in range(train_v_iters):
+            sess.run(train_v, feed_dict=inputs)
+
+        # Log changes
+        pi_l_new, v_l_new, kl, cf = sess.run([pi_loss, v_loss, approx_ent], feed_dict=inputs)
+        logger.store(LossPi=pi_l_old, LossV=v_l_old, KL=kl, Entropy=ent, ClipFrac=cf,
+                     DeltaLossPi=(pi_l_new - pi_l_old), DeltaLossV=(v_l_new - v_l_old))
+
+    start = time.time()
+    o, r, d, ep_ret, ep_len = env.reset(), 00, False, 0, 0
+
+    # MAIN LOOP
+    for epoch in range(epochs):
+        for t in range(local_steps_per_epoch):
+            a, v_t, logp_t = sess.run(get_action_ops, feed_dict={x_ph: o.reshape(1, -1)})
+
+            # Save and log
+            buf.store(o, a, r, v_t, logp_t)
+            logger.store(VVals=v_t)
+
+            o, r, d, _ = env.step(a[0])
+            ep_ret += 1
+            ep_len += 1
+
+            terminal = d or (ep_len == max_ep_len)
+            if terminal or (t == local_steps_per_epoch):
+                if not terminal:
+                    print('Warning: trajectory cut off by epoch at {} steps'.format(ep_len))
+                last_val = r if d else sess.run(v, feed_dict={x_ph: o.reshape(1, -1)})
+                buf.finish_path(last_val)
+                if terminal:
+                    logger.store(EpRet=ep_ret, EpLen=ep_len)
+                o, r, d, ep_ret, ep_len = env.reset(), 0, False, 0, 0
+
+        # Save model
+        if (epoch % save_freq == 0) or (epoch == epochs - 1):
+            logger.save_state({'env': env}, None)
+
+        update()
+
+        # Log info about epoch
+        logger.log_tabular('Epoch', epoch)
+        logger.log_tabular('EpRet', with_min_and_max=True)
+        logger.log_tabular('EpLen', average_only=True)
+        logger.log_tabular('VVals', with_min_and_max=True)
+        logger.log_tabular('TotalEnvInteracts', (epoch - 1) * steps_per_epoch)
+        logger.log_tabular('LossPi', average_only=True)
+        logger.log_tabular('LossV', average_only=True)
+        logger.log_tabular('DeltaLossPi', average_only=True)
+        logger.log_tabular('DeltaLossV', average_only=True)
+        logger.log_tabular('Entropy', average_only=True)
+        logger.log_tabular('KL', average_only=True)
+        logger.log_tabular('ClipFrac', average_only=True)
+        logger.log_tabular('StopIter', average_only=True)
+        logger.log_tabular('Time', time.time() - start)
+        logger.dump_tabular()
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env', type=str, default='HalfCheetah-v2')
+    parser.add_argument('--hid', type=int, default=64)
+    parser.add_argument('--l', type=int, default=2)
+    parser.add_argument('--gamma', type=float, default=0.99)
+    parser.add_argument('--seed', '-s', type=int, default=0)
+    parser.add_argument('--cpu', type=int, default=4)
+    parser.add_argument('--steps', type=int, default=4000)
+    parser.add_argument('--epochs', type=int, default=50)
+    parser.add_argument('--exp_name', type=str, default='ppo')
+    args = parser.parse_args()
+
+    mpi_fork(args.cpu)  # run parallel code with mpi
+
+    from first_round.run_utils import setup_logger_kwargs
+
+    logger_kwargs = setup_logger_kwargs(args.exp_name, args.seed)
+
+    ppo(lambda: gym.make(args.env), actor_critic=core.mlp_actor_critic,
+        ac_kwargs=dict(hid_size=[args.hid] * args.l), gamma=args.gamma,
+        seed=args.seed, steps_per_epoch=args.steps, epochs=args.epochs,
+        logger_kwargs=logger_kwargs)
